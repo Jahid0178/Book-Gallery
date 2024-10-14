8188bdb77287a3837a47aae29c56f4d6fd32066b
Jahidul <jahidulhaque767@gmail.com>
2024-10-14 17:56:25 +0600

    feat: add render books card from api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-console.log("JS Connected");
+const bookWrapper = document.querySelector(".books-list-wrapper");
+
+// Render Books
+function renderBooks(books) {
+  books.forEach((book) => {
+    const bookLink = document.createElement("a");
+    bookLink.href = `./book.html/${book.id}`;
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.innerHTML = `
+      <img
+        src="${book.formats["image/jpeg"]}"
+        alt="${book.title}"
+        class="book-cover"
+      />
+      <h2 class="book-title">${book.title}</h2>
+      <div class="book-details">
+        <p>Book ID: ${book.id}</p>
+        <p>Genre: Fantasy</p>
+      </div>
+      <div class="author-info">
+        <p>Author: ${book.authors[0]?.name || "Unknown"}</p>
+      </div>
+    `;
+    bookLink.appendChild(bookCard);
+    bookWrapper.appendChild(bookLink);
+  });
+}
+
+const response = fetch(
+  "https://gutendex.com/books?page=1&language=en&per_page=10"
+);
+
+response
+  .then((response) => response.json())
+  .then((data) => {
+    renderBooks(data.results);
+  });
